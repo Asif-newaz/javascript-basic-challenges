b5bd10c06ed94abf90ccb08d128c79e49bea78ba
Asif-newaz <asif.newaz0093@gmail.com>
2020-07-22 01:51:05 +0600

    started workging with challenge 5 part-2

--- script.js
+++ script.js
@@ -294,19 +294,39 @@ var john = {
 }
 
 
-//john.calcTips();
-//console.log(john);
+var mark = {
+    fullName: 'Mark Miller',
+    bills: [77, 375, 110, 45],
+    calcTips: function() {
+        this.tips = [];
+        this.finalValues = [];
+         
+        for (var i = 0; i < this.bills.length; i++) {
+            
+            // Determine percentage based on tipping rules
+            var percentage;
+            var bill = this.bills[i];
+            
+            if (bill < 100) {
+                percentage = 20/100;
+            } else if (bill >= 100 && bill <= 300) {
+                percentage = 10/100;
+            } else {
+                percentage = 25/100;
+            }
+            
+            
+            // Add results to the corresponding arrays
+            this.tips[i] = bill * percentage;
+            this.finalValues[i] = bill + bill * percentage;
+        }
+    }
+}
 
-console.log(john.calcTips());
+john.calcTips();
+mark.calcTips();
 
-
-
-
-
-
-
-
-
+console.log(john, mark);
 
 
 
