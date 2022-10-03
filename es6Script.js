bf4534a57911fcc607894384c72ab798d716a67b
Asif-newaz <asif.newaz0093@gmail.com>
2022-10-04 03:19:34 +0600

    work on challenge 4

--- es6Script.js
+++ es6Script.js
@@ -82,39 +82,67 @@ GOOD LUCK 😀
 // console.log(total);
 
 //Challenge 3
-const mark = {
-  fullName: "Mark",
-  mass: 78,
-  height: 1.69,
+// const mark = {
+//   fullName: "Mark",
+//   mass: 78,
+//   height: 1.69,
 
-  calcBMI: function () {
-    this.BMI = this.mass / (this.height * this.height);
-    return this.BMI;
-  },
+//   calcBMI: function () {
+//     this.BMI = this.mass / (this.height * this.height);
+//     return this.BMI;
+//   },
+// };
+
+// const john = {
+//   fullName: "John",
+//   mass: 92,
+//   height: 1.95,
+
+//   calcBMI: function () {
+//     this.BMI = this.mass / (this.height * this.height);
+//     return this.BMI;
+//   },
+// };
+
+// const markBMI = mark.calcBMI();
+// const johnBMI = john.calcBMI();
+
+// console.log(john.BMI);
+
+// if (mark.BMI > john.BMI) {
+//   console.log(
+//     `${mark.fullName}'s BMI (${mark.BMI}) is higher than ${john.fullName}'s BMI!`
+//   );
+// } else {
+//   console.log(
+//     `${john.fullName}'s BMI (${john.BMI}) is higher than ${mark.fullName}'s BMI!`
+//   );
+// }
+
+//Challenge 4
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+const tipsAndTotals = [];
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? (15 / 100) * bill : (20 / 100) * bill;
 };
 
-const john = {
-  fullName: "John",
-  mass: 92,
-  height: 1.95,
+// console.log(calcTip(bills[0]));
 
-  calcBMI: function () {
-    this.BMI = this.mass / (this.height * this.height);
-    return this.BMI;
-  },
-};
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// console.log(tips);
 
-const markBMI = mark.calcBMI();
-const johnBMI = john.calcBMI();
+// const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 
-console.log(john.BMI);
+// console.log(total);
 
-if (mark.BMI > john.BMI) {
-  console.log(
-    `${mark.fullName}'s BMI (${mark.BMI}) is higher than ${john.fullName}'s BMI!`
-  );
-} else {
-  console.log(
-    `${john.fullName}'s BMI (${john.BMI}) is higher than ${mark.fullName}'s BMI!`
-  );
+// tipsAndTotals[0] = total;
+// console.log(tipsAndTotals);
+
+for (let i = 0; i <= bills.length - 1; i++) {
+  const tips = calcTip(bills[i]);
+  tipsAndTotals[i] = tips + bills[i];
 }
+
+console.log(tipsAndTotals);
